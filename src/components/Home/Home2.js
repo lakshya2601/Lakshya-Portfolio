2fabd333fc01e7e11f2158baa9c7d07a206a6397
lakshya2601 <89459982+lakshya2601@users.noreply.github.com>
2022-12-30 21:15:19 +0530

    Add files via upload

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import { AiFillGithub, AiOutlineTwitter } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              I fell in love with programming and I have at least learnt
+              something, I think… 🤷‍♂️
+              <p>
+                <br /> I'm an Indian frontend developer and incredibly
+                passionate and committed to what I do. I am currently attending
+                <i>
+                  <b className="purple">
+                    {" "}
+                    Shri Vaishnav Vidyapeeth Vishwavidyalaya Indore{" "}
+                  </b>
+                </i>
+                to get my bachelor's degree in computer science. Being artistic,
+                I enjoy making drawings,sketches & playing cricket in my spare
+                time. I enjoy taking walks and listening to music. I often want
+                to learn something new.
+              </p>
+              <br />I am fluent in classics like
+              <i>
+                <b className="purple"> CSS, React.js and JavaScript. </b>
+              </i>
+              <br />
+              <br />
+              My field of Interest's are building new &nbsp;
+              <i>
+                <b className="purple">Web Sites </b>
+              </i>
+              <br />
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/lakshya2601"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/lakshyaporwal2601/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/lakshya_2601"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
